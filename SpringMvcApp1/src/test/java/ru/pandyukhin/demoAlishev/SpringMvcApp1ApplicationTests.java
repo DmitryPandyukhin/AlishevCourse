ba9148427337bb9b65a64e6a76e632d1ff31be10
Dmitry Pandyukhin <pandyukhim@gmail.com>
2024-03-05 15:04:15 +0300

    Создан проект. Реализовано получение и добавление людей.

---
+++ SpringMvcApp1/src/test/java/ru/pandyukhin/demoAlishev/SpringMvcApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.pandyukhin.demoAlishev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
